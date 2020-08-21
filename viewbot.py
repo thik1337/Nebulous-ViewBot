fd6584cf17bb1bafdfc344c8314c5d7457ef12bf
Anthy1x <70001027+Anthy1x@users.noreply.github.com>
2020-08-21 08:40:55 -0700

    Will now show views gained and views gained per second. Bug fixes, error checks.

--- viewbot.py
+++ viewbot.py
@@ -4,7 +4,7 @@ import time
 
 from urllib3 import PoolManager
 from urllib.parse import urlencode
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 HOST = "https://www.simplicialsoftware.com"
 GET_PLAYER_PROFILE_URI = "/api/account/GetPlayerProfile"
@@ -14,7 +14,7 @@ pdata_get_player_profile = {
     'Game': 'Nebulous',
     'Version': 1065,
     'Ticket': LOGIN_TICKET,
-    'accountID': 0
+    'accountID': 100
 }
 headers = {
     'Accept-Encoding': 'gzip',
@@ -28,10 +28,15 @@ work = True
 lock = threading.Lock()
 total_views = 0
 
+# not really the best way to validate but it should get the job done
+def invalid_ticket():
+    resp = manager.urlopen('POST', HOST + GET_PLAYER_PROFILE_URI, body = urlencode(pdata_get_player_profile))
+    data = json.loads(resp.data.decode('utf-8'))
+
+    return 'Message' in data and data['Message'] == "An error has occurred."
+
 def do_work(quota):
     global total_views
-
-    _quota = quota
 
     method = 'POST'
     body = urlencode(pdata_get_player_profile)
@@ -40,10 +45,23 @@ def do_work(quota):
     while work and quota > 0:
         manager.urlopen(method, url, body = body)
 
+        with lock:
+            total_views += 1
+
         quota -= 1
 
-    with lock:
-        total_views += _quota - quota
+def print_stats(quota):
+    global total_views
+
+    old_views = 0
+
+    while work and total_views != quota:
+        with lock:
+            print("[ + ] Gained: %d(%d/s)" % (total_views, max(total_views - old_views, 0)), end = '\r')
+
+            old_views = total_views
+
+        time.sleep(1)
 
 def main():
     global work
@@ -55,10 +73,26 @@ def main():
     args = parser.parse_args()
     threshold = args.views
 
+    if not LOGIN_TICKET:
+        print("[ ! ] LOGIN_TICKET is empty. Please add a login ticket!")
+
+        return
+
+    if invalid_ticket():
+        print("[ ! ] Invalid login ticket! Make sure there are no extra/missing characters in the login ticket.")
+
+        return
+
+    if args.account_id == int(LOGIN_TICKET.split(',')[0]):
+        print("[ ! ] You can't give yourself views. Please use a different login ticket if you wish to give yourself views.")
+
+        return
+
     print("[ + ] Running %d bots for user '%d'..." % (args.threads, args.account_id))
 
     pdata_get_player_profile['accountID'] = args.account_id
     work_quota, rem = divmod(args.views, args.threads)
+    stat_thread = threading.Thread(target = print_stats, args = [args.views - rem])
     workers = []
 
     try:
@@ -70,6 +104,9 @@ def main():
             worker.start()
             workers.append(worker)
 
+        stat_thread.daemon = True
+        stat_thread.start()
+
         while (total_views + rem) != args.views:
             time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
@@ -78,10 +115,12 @@ def main():
         for worker in workers:
             worker.join()
 
-        print("[ + ] Total views gained: %d." % total_views)
+        stat_thread.join()
+
+        print("\n[ + ] Total views gained: %d." % total_views)
 
         return
 
-    print("[ + ] Done.")
+    print("\n[ + ] Done.")
 
 main()
